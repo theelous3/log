0128705a92e2dcd128add3b015acff162d1a07d1
Carl Bordum Hansen <carl@bordum.dk>
2017-07-02 19:04:52 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='log',
+    version='0.0.1',
+    author='Carl Bordum Hansen',
+    author_email='carl@bordum.dk',
+    license='MIT',
+    description='Pythonic logging with familiar API.',
+    url='https://github.com/Zaab1t/log',
+    packages=['log'],
+)
